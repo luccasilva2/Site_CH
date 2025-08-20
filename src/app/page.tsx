35ebfe008f492ca35d15e0aa17ec7b0acbf88378
luccasilva2 <silvaoliveiralucca@gmail.com>
2025-08-20 18:13:16 -0300

    Commit de atualização do mapa e dos respectivos países

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import { Header } from '@/components/layout/header';
 import HeroWithImageBackground from '@/components/sections/hero-with-image-background';
+import MapSection from '@/components/sections/map-section';
 import ArsenalChartSection from '@/components/sections/arsenal-chart-section';
 import TimelineSection from '@/components/sections/timeline-section';
 import FactsSection from '@/components/sections/facts-section';
 import InfographicsSection from '@/components/sections/infographics-section';
-import MapSection from '@/components/sections/map-section';
 
 export default function Home() {
   return (
@@ -12,11 +12,11 @@ export default function Home() {
       <Header />
       <main>
         <HeroWithImageBackground />
+        <MapSection />
         <ArsenalChartSection />
         <TimelineSection />
         <FactsSection />
         <InfographicsSection />
-        <MapSection />
       </main>
     </div>
   );
